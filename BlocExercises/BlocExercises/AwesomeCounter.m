fd629bf68ce6edffa2d305978eb331b2d162b151
Marius Horga <mhorga@gmail.com>
2014-11-07 14:58:06 +0200

    Completed loops - revised

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -13,14 +13,13 @@
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
     NSString *numberString = @"";
-    if (number <= otherNumber) {
-        for (NSInteger i=number; i<=otherNumber; i++) {
-            numberString = [NSString stringWithFormat:@"%@%ld", numberString, (long)i];
-        }
-    } else {
-        for (NSInteger i=otherNumber; i<=number; i++) {
-            numberString = [NSString stringWithFormat:@"%@%ld", numberString, (long)i];
-        }
+    if (number > otherNumber) {
+        NSInteger temp = number;
+        number = otherNumber;
+        otherNumber = temp;
+    }
+    for (NSInteger i=number; i<=otherNumber; i++) {
+        numberString = [NSString stringWithFormat:@"%@%ld", numberString, (long)i];
     }
     
     return numberString;
